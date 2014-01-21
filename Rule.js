9cbb56fa8fd629a44722e9d26856e9a37b856a31
Florian Cargoët <florian.cargoet@gmail.com>
2014-01-21 09:09:54 +0100

    init

---
+++ Rule.js
@@ -0,0 +1,32 @@
+var Rule = module.exports = function Rule(command, selector, body) {
+  this.command = command;
+  this.selector = selector;
+  this.body = body;
+  console.log(this)
+};
+
+Rule.fromXml = function fromXml(node) {
+  return new Rule(node.name(), node.attr('selector').value(), node.childNodes().map(String).join(''));
+};
+
+Rule.prototype.apply = function apply(document) {
+  var self = this;
+  if (this.command in this) {
+    Array.prototype.forEach.call(document.querySelectorAll(this.selector), function (node) {
+      self[self.command](node, document);
+    });
+  }
+};
+
+Rule.prototype.remove = function remove(node, document) {
+  node.parentNode.removeChild(node);
+};
+
+Rule.prototype.append = function append(node, document) {
+  var container = document.createElement('div');
+  container.innerHTML = this.body;
+  var children = container.childNodes
+  while(children.length) {
+    node.appendChild(children[0]);
+  }
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var jsdom = require('jsdom');
+var xml = require('libxmljs');
+
+var Rule = require('./Rule');
+
+var inputFile = fs.readFileSync('./test.html', 'utf8');
+var rulesFile = fs.readFileSync('./test.xml', 'utf8');
+
+var rules = xml.parseXml(rulesFile).find('/rules/*').map(Rule.fromXml);
+
+console.log(inputFile);
+
+jsdom.env(inputFile, function (errors, window) {
+  rules.forEach(function (rule) {
+    rule.apply(window.document);
+  });
+  var html = toHTML(window.document);
+  console.log(html);
+});
+
+function toHTML(doc) {
+  var html = '';
+
+  if (doc.doctype) {
+    html += String(doc.doctype);
+  }
+  html += doc.innerHTML;
+  return html;
+}
